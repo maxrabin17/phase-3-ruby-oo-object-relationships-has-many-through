03e7d3b0d7dd8099daafa0dbb7a41d4c71763aee
maxrabin <max.rabin17@gmail.com>
2021-07-03 13:01:37 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class Customer
-  
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+        # save
+    end
+
+    # def save
+    #     @@all << self
+    # end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.collect {|meal| meal.waiter}
+        # binding.pry
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+    attr_accessor :name, :y_o_e
 
+    @@all = []
+
+    def initialize(name, y_o_e)
+        @name = name
+        @y_o_e = y_o_e
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        tipped = meals.max {|a, b| a.tip <=> b.tip}
+        tipped.customer
+    end
 end
